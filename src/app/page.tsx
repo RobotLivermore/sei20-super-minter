e57757d61d3d6bac48df0e9355acc0e1da02d826
Robot.l <lbh9311@sina.com>
2023-12-17 01:10:34 +0800

    feat: add share code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import Minter from "@/components/Minter";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen items-center justify-between p-24">
+      <div className="flex flex-col">
+        <h1 className="text-4xl font-bold">欢迎加入科学家的队伍</h1>
+        <p className="text-xs mt-2 text-gray-400">进群免费领工具</p>
+        <Image src="/shareQrcode.jpeg" width={360} alt="" height={400} />
+      </div>
       <Minter />
     </main>
   );
