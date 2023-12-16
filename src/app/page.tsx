a63f90c30f22b2d9f46a1617a3543a72b7b3155b
Robot.l <lbh9311@sina.com>
2023-12-17 01:14:14 +0800

    fix: ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-between p-24">
+    <main className="flex min-h-screen items-center justify-center space-x-48 p-24">
       <div className="flex flex-col">
         <h1 className="text-4xl font-bold">欢迎加入科学家的队伍</h1>
         <p className="text-xs mt-2 text-gray-400">进群免费领工具</p>
