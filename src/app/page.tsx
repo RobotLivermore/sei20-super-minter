309b213db0628186be634cc2abf5a5675b62b106
Robot.l <lbh9311@sina.com>
2023-12-17 09:36:19 +0800

    feat: add github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,11 @@ export default function Home() {
         <h1 className="text-4xl font-bold">欢迎加入科学家的队伍</h1>
         <p className="text-xs mt-2 text-gray-400">进群免费领工具</p>
         <Image src="/shareQrcode.jpeg" width={360} alt="" height={400} />
-        <Link href={`https://twitter.com/LewisLLL1`} target="_blank" className="flex items-center justify-center">
+        <Link
+          href={`https://twitter.com/LewisLLL1`}
+          target="_blank"
+          className="flex items-center justify-center"
+        >
           <svg
             className="w-4 h-4 mr-2"
             aria-hidden="true"
@@ -23,6 +27,26 @@ export default function Home() {
             />
           </svg>
           <span className="underline">关注科学家</span>
+        </Link>
+        <Link
+          href={`https://github.com/BohengLiu/sei20-super-minter`}
+          target="_blank"
+          className="flex items-center justify-center"
+        >
+          <svg
+            className="w-6 h-6 text-gray-800 mr-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <span className="underline">开源安全可直接下载代码</span>
         </Link>
       </div>
       <Minter />
